f08926f85bbf23e512e96e4887040869ed351e7c
Amir Asaad <asaad.amir@icloud.com>
2020-09-13 15:41:23 +0200

    Fix Paste factory in tests

--- pastebin/pastes/tests/factories.py
+++ pastebin/pastes/tests/factories.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
-from factory import DjangoModelFactory, Faker, LazyFunction
+from factory import DjangoModelFactory, LazyFunction
 
 from pastebin.pastes.models import Paste
 
 
 class PasteFactory(DjangoModelFactory):
-    content = Faker('content')
     created = LazyFunction(datetime.now)
 
     class Meta:
